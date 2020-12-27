796816d34a82937a824f227b096f107506371aa6
Chris <chriscrutt@gmail.com>
2020-12-27 01:22:34 -0800

    more public funcs

--- contracts/token/ERC2718/ERC2718.sol
+++ contracts/token/ERC2718/ERC2718.sol
@@ -14,8 +14,40 @@ contract ERC2718 is Context {
 
     mapping(address => Bundle) private _balances;
 
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
+
     event Invested(address account, uint256 shares);
     event Voted(address account, uint256 votes);
+
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+    }
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        Bundle storage b = _balances[account];
+        return b.shares;
+    }
+
+    function votesLeft(address account) public view returns (uint256) {
+        Bundle storage b = _balances[account];
+        return b.votesLeft;
+    }
 
     function invest(uint256 shares) public returns (bool) {
         _invest(_msgSender(), shares);
@@ -32,6 +64,7 @@ contract ERC2718 is Context {
         require(_shares > 0, "must buy more than 0 shares lol");
         Bundle storage b = _balances[_owner];
 
+        _totalSupply = _totalSupply.add(_shares);
         b.shares = b.shares.add(_shares);
         b.votesLeft = b.votesLeft.add(_shares);
 
