212b07a4b13ce5c4fa004f44b36f758317c7e9a0
Chris <chriscrutt@gmail.com>
2021-01-08 23:27:26 -0800

    finishing touch ups

--- contracts/wallet/wallet.sol
+++ contracts/wallet/wallet.sol
@@ -2,48 +2,43 @@
 
 pragma solidity >=0.6.0 <0.9.0;
 
-import "../access/roles/HolderRole.sol";
 import "../utils/Context.sol";
 import "../token/ERC20/IERC20.sol";
 import "../math/SafeMath.sol";
 
-contract MultiSig is Context, HolderRole {
+contract MultiSigCopy is Context {
     using SafeMath for uint256;
-
-    struct Holder {
-        address holder;
-        bool signed;
-    }
 
     struct Transaction {
         uint256 amount;
-        address coinAddress;
+        IERC20 coinAddress;
         address sendTo;
         uint256 goodTillTime;
     }
 
-    Holder private _holder;
-    Holder[] private _holders;
+    mapping(address => uint256) private _holderSignTime;
+
+    address private _owner;
 
     Transaction private _transaction;
 
     IERC20 private _coin;
 
-    uint256 private _signatureNum;
+    uint8 private _numberHolders;
+    uint8 private _signatureNum;
 
-    event Initialized(address, Transaction);
-    event Signed(address);
-    event Completed(address, Transaction);
-    event Received(address, uint256);
+    event Initialized(address indexed, Transaction);
+    event Signed(address indexed);
+    event Completed(address indexed, Transaction);
+    event Received(address indexed, uint256);
 
     constructor(address[] memory holders) {
-        Holder memory h = _holder;
-        for (uint256 i = 0; i < holders.length; i++) {
+        _owner = _msgSender();
+        uint256 time = block.timestamp;
+        for (uint8 i = 0; i < holders.length; i++) {
             require(holders[i] != address(0), "0 address can't be a holder");
-            h.holder = holders[i];
-            h.signed = false;
-            _holders.push(h);
-            _addHolder(holders[i]);
+
+            _holderSignTime[holders[i]] = time;
         }
     }
 
@@ -51,50 +46,89 @@ contract MultiSig is Context, HolderRole {
         emit Received(_msgSender(), msg.value);
     }
 
-    function initiateTransfer(
-        uint256 amount,
-        address coinAddress,
-        address sendTo
-    ) public onlyHolder {
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    function addHolder(address account) public {
+        require(_msgSender() == _owner, "not owner");
+        require(account != address(0), "0 address can't be a holder");
+
+        _addHolder(account);
+    }
+
+    function replaceHolder(address currentAccount, address newAccount) public {
+        require(_msgSender() == _owner, "not owner");
+        require(newAccount != address(0), "0 address can't be a holder");
+
+        _holderSignTime[currentAccount] = 0;
+        _addHolder(newAccount);
+    }
+
+    function initiateTransfer(uint256 amount, address sendTo) public {
         require(amount > 0, "transfer amount can't be zero");
         require(sendTo != address(0), "can't send to 0 address");
-        // if (coinAddress == address(0)) {
-        //     _initiateTransfer(
-        //         amount,
-        //         coinAddress,
-        //         sendTo,
-        //         block.timestamp.add(10 minutes)
-        //     );
-        // }
+        require(_holderSignTime[_msgSender()] > 0, "not a holder, jerk");
+        require(
+            block.timestamp > _transaction.goodTillTime,
+            "wait for old transaction"
+        );
+        require(address(this).balance >= amount, "not enough balance");
 
         _initiateTransfer(
             amount,
-            IERC20(coinAddress),
+            address(0),
             sendTo,
             block.timestamp.add(10 minutes)
         );
     }
 
-    function sign() public onlyHolder {
+    function initiateTransfer(
+        uint256 amount,
+        address sendTo,
+        address coinAddressIfApplicable
+    ) public {
+        require(amount > 0, "transfer amount can't be zero");
+        require(sendTo != address(0), "can't send to 0 address");
+        require(_holderSignTime[_msgSender()] > 0, "not a holder, jerk");
+        require(
+            block.timestamp > _transaction.goodTillTime,
+            "wait for old transaction"
+        );
+        require(
+            IERC20(coinAddressIfApplicable).balanceOf(address(this)) >= amount,
+            "not enough balance"
+        );
+
+        _initiateTransfer(
+            amount,
+            coinAddressIfApplicable,
+            sendTo,
+            block.timestamp.add(10 minutes)
+        );
+    }
+
+    function sign() public {
+        require(_holderSignTime[_msgSender()] > 0, "not a holder, jerk");
         require(!_hasSigned(_msgSender()), "already signed");
+
         _sign(_msgSender());
     }
 
-    function completeTransfer(
-        uint256 amount,
-        address coinAddress,
-        address sendTo
-    ) public onlyHolder {
-        Transaction memory t = _transaction;
-        require(_signatureNum > _holders.length.div(2), "over half must sign");
-        require(amount == t.amount, "amounts not equal");
+    function completeTransfer(uint256 amount, address sendTo) public {
+        require(_holderSignTime[_msgSender()] > 0, "not a holder, jerk");
         require(
-            coinAddress == address(t.coinAddress),
-            "coin addresses not equal"
+            _signatureNum > uint256(_numberHolders).div(2),
+            "over half must sign"
         );
+
+        Transaction memory t = _transaction;
+
+        require(amount == t.amount, "amounts not equal");
         require(sendTo == t.sendTo, "recipient address not equal");
         require(_timeLeftSeconds() > 0, "time is up! restart sorry");
-        _completeTransfer(amount, IERC20(coinAddress), sendTo);
+
+        _completeTransfer(amount, t.coinAddress, sendTo);
     }
 
     function currentTransaction() public view returns (Transaction memory) {
@@ -105,7 +139,7 @@ contract MultiSig is Context, HolderRole {
         return _hasSigned(account);
     }
 
-    function numSigned() public view returns (uint256) {
+    function numSigned() public view returns (uint8) {
         return _signatureNum;
     }
 
@@ -113,26 +147,43 @@ contract MultiSig is Context, HolderRole {
         return _timeLeftSeconds();
     }
 
+    function _addHolder(address _account) private {
+        _holderSignTime[_account] = block.timestamp;
+    }
+
     function _initiateTransfer(
         uint256 _amount,
-        IERC20 _coinAddress,
+        address _coinAddress,
         address _sendTo,
         uint256 _goodTillTime
     ) private {
-        Transaction memory t = _transaction;
+        Transaction storage t = _transaction;
         t.amount = _amount;
-        t.coinAddress = address(_coinAddress);
+        if (_coinAddress != address(0)) {
+            t.coinAddress = IERC20(_coinAddress);
+        }
         t.sendTo = _sendTo;
         t.goodTillTime = _goodTillTime;
-        _transaction = t;
         emit Initialized(_msgSender(), t);
     }
 
+    function _hasSigned(address _account) private view returns (bool) {
+        Transaction memory t = _transaction;
+
+        return
+            t.goodTillTime - 10 minutes <= _holderSignTime[_account] &&
+            _holderSignTime[_account] <= t.goodTillTime;
+    }
+
     function _sign(address _account) private {
-        uint8 i = _getAccountNum(_account);
-        _holders[i].signed = true;
+        _holderSignTime[_account] = block.timestamp;
         _signatureNum++;
         emit Signed(_account);
+    }
+
+    function _timeLeftSeconds() private view returns (uint256) {
+        Transaction memory t = _transaction;
+        return t.goodTillTime.sub(block.timestamp, "over time limit");
     }
 
     function _completeTransfer(
@@ -140,10 +191,8 @@ contract MultiSig is Context, HolderRole {
         IERC20 _coinAddress,
         address _sendTo
     ) private {
-        Holder[] memory _h = _holders;
-        for (uint256 i = 0; i < _h.length; i++) {
-            _h[i].signed = false;
-        }
+        Transaction memory t = _transaction;
+        delete _transaction;
 
         if (address(_coinAddress) == address(0)) {
             payable(_sendTo).transfer(_amount);
@@ -151,26 +200,7 @@ contract MultiSig is Context, HolderRole {
             _coin = _coinAddress;
             _coin.transfer(_sendTo, _amount);
         }
-        emit Completed(_msgSender(), _transaction);
-    }
 
-    function _getAccountNum(address _account) private view returns (uint8 i) {
-        Holder[] memory _h = _holders;
-        for (i = 0; i < _h.length; i++) {
-            if (_h[i].holder == _account) {
-                return i;
-            }
-        }
-    }
-
-    function _hasSigned(address _account) private view returns (bool) {
-        uint8 i = _getAccountNum(_account);
-        return _holders[i].signed;
-    }
-
-    function _timeLeftSeconds() private view returns (uint256) {
-        Transaction storage t = _transaction;
-        uint256 time = t.goodTillTime;
-        return time.sub(block.timestamp, "over time limit");
+        emit Completed(_msgSender(), t);
     }
 }
